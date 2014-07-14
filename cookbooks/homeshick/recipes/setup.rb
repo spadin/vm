45d841894471d80e9fe161c5ae1d740f36ac7d66
Sandro Padin <sandropadin@gmail.com>
2014-07-14 16:32:54 -0500

    setting dotfiles repo and remote

--- cookbooks/homeshick/recipes/setup.rb
+++ cookbooks/homeshick/recipes/setup.rb
@@ -3,9 +3,17 @@ homeshick_sh = Helper.home('.homesick/repos/homeshick/homeshick.sh')
 user_bash 'configure homeshick castles' do
   code <<-EOF
     source #{homeshick_sh}
-    homeshick clone git@github.com:spadin/dotfiles.git
+    homeshick clone spadin/dotfiles
     homeshick clone jimeh/tmuxifier
     homeshick clone gmarik/vundle
     homeshick link -f
+  EOF
+end
+
+user_bash 'fix spadin/dotfiles remote' do
+  code <<-EOF
+    homeshick cd dotfiles
+    git remote rm origin
+    git remote add origin git@github.com:spadin/dotfiles.git
   EOF
 end
